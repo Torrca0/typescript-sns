994ebba29a7ca0c0468cdadf836e6a4db08deb24
Torrca <yuki13921@gmail.com>
2023-02-10 22:54:11 +0900

    エラーはいた助けて

--- server.ts
+++ server.ts
@@ -4,8 +4,18 @@ const userRoute =require("./routes/users");
 const authRoute = require("./routes/auth");
 const postsRoute = require("./routes/posts")
 const PORT :number = 3000;
+require("dotenv").config();
 
 const app = express();
+
+mongoose
+    .connect(process.env.MONGOURL)
+    .then(() => {
+        console.log("connecting DB");
+    })
+    .catch((err :any) => {
+        console.log("error");
+    })
 
 app.use(express.json());
 app.use("/apt/users", userRoute);
